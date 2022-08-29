79160c3f8e1c937acf23cfe96030cbc588bcdf6b
Matheus Rocha <matheusrrocha@gmail.com>
2022-08-29 12:40:54 +0100

    update page

--- vendor/plugins/shuffle/intro.js
+++
@@ -1,9 +0,0 @@
-(function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['jquery', 'modernizr'], factory);
-  } else {
-    window.Shuffle = factory(window.jQuery, window.Modernizr);
-  }
-})(function($, Modernizr, undefined) {
-
-'use strict';
